2b4c2145af28c8428b39c65d7be965eadabe5090
umesh725121 <108479549+umesh725121@users.noreply.github.com>
2023-08-04 10:06:38 +0530

    Add files via upload

--- function.py
+++ function.py
@@ -5,14 +5,9 @@ import panel as pn
 import openai
 import streamlit as st
 
-
-from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
 
-from langchain.vectorstores import Milvus
-from langchain.document_loaders import TextLoader
-from langchain.agents import load_tools
 from langchain.sql_database import SQLDatabase
 from langchain.agents import *
 from langchain.llms import OpenAI
@@ -21,28 +16,18 @@ from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.chat_models import ChatOpenAI
 
 
+
 # Setup Keys and DB connection 
-os.environ['OPENAI_API_KEY'] = ''
-#os.environ['SERPAPI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 db = SQLDatabase.from_uri("postgresql://postgres:postgres@localhost:5432/HR")
 
 
 
-#llm = ChatOpenAI(model_name="gpt-3.5-turbo")
-
-# setup LLM and SQL toolkit 
-#llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
-#toolkit = SQLDatabaseToolkit(db=db,llm=llm)
-#agent_executor = create_sql_agent(
- #   llm=llm,
-  #  toolkit=toolkit,
-   # verbose=True
-#)
-#response = agent_executor.run('list all employees who work in london')
-
 
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
 
+# The agent builds off of SQLDatabaseChain and is designed to answer more general questions about a database
+#  as well as recover from errors.
 toolkit = SQLDatabaseToolkit(db=db,llm=llm)
 
 def sql_gen(question):
@@ -52,7 +37,6 @@ def sql_gen(question):
     verbose=True
 )
     return agent_executor.run(question)
-#question = "List all the jobs"
 
-#path = st.text_input(question)
-st.write(sql_gen(st.text_input("enter the statement")))
+#integrate with Sreamlit
+st.write(sql_gen(st.text_input("enter the")))
